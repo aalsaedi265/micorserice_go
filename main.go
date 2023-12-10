0869100e6d41b825080e9a168ab0c77a43972c7d
Ahmed Alsaedi <aalsaedi265@gmail.com>
2023-12-10 16:20:26 -0600

    graceful shutdonw completed

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	// "fmt"
 	// "io/ioutil"
+	"context"
 	"log"
 	"microserices_go/handlers"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
@@ -17,6 +20,29 @@ func main() {
 	sm := http.NewServeMux()
 	sm.Handle("/", hh)
 	sm.Handle("/goodbye", gh)
-	http.ListenAndServe(":9090", sm)
+	s := &http.Server{
+		Addr: ":9090",
+		Handler: sm,
+		IdleTimeout: 120* time.Second,
+		ReadTimeout: 1*time.Second,
+		WriteTimeout: 1*time.Second,
+	}
+	
+	go func(){
+		err := s.ListenAndServe()
+		if err != nil{
+			l.Printf("Error starting server: %s\n", err)
+			os.Exit(1)
+		}
+	}()
+	
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, os.Kill)
 
+	sig := <-c //blocks and waits for a signal to be sent to the channe
+	log.Println("Got signal: ", sig)
+	// http.ListenAndServe(":9090", sm)
+	tc,_ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 }
