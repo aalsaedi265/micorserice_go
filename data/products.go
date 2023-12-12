59cb5337dc83744f9ef92ebc0abc1adb3973176f
Ahmed Alsaedi <aalsaedi265@gmail.com>
2023-12-12 11:15:24 -0600

    bare min

--- data/products.go
+++ data/products.go
@@ -1,6 +1,10 @@
 package data
 
-import "time"
+import (
+	"encoding/json"
+	"io"
+	"time"
+)
 
 type Product struct{
 	ID          int     `json:"id"`
@@ -13,7 +17,15 @@ type Product struct{
 	DeletedOn   string  `json:"-"`
 }
 
-func GetProducts()[]*Product{
+type Products []*Product
+
+func (p *Products) ToJSON(w io.Writer) error {
+	e := json.NewEncoder(w)
+	return e.Encode(p)
+}
+
+// GetProducts returns a list of products
+func GetProducts() Products {
 	return productList
 }
 
--- handlers/products.go
+++ handlers/products.go
@@ -1,25 +1,32 @@
 package handlers
 
 import (
-	"encoding/json"
+	// "encoding/json"
 	"log"
 	"microserices_go/data"
 	"net/http"
 )
 
-type Product struct{
+type Products struct{
 	l*log.Logger
 }
 
-func NewProducts(l*log.Logger)*Product{
-	return &Product{l}
+func NewProducts(l*log.Logger)*Products{
+	return &Products{l}
 }
 
-func(p*Product) ServeHTTP(rw http.ResponseWriter, h*http.Request){
+func(p*Products) ServeHTTP(rw http.ResponseWriter, r*http.Request){
+	if r.Method == http.MethodGet{
+		p.GetProducts(rw, r)
+		return
+	}
+	rw.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+func (p*Products) GetProducts(rw http.ResponseWriter, h*http.Request){
 	lp:= data.GetProducts()
-	d, err := json.Marshal(lp)
+	err := lp.ToJSON(rw)
 	if err != nil{
 		http.Error(rw, "unable to marshal the json", http.StatusInternalServerError)
 	}
-	rw.Write(d)
 }
