b1d4c15a5d295128c33dd3e677ce00fb23b62461
Ahmed Alsaedi <aalsaedi265@gmail.com>
2023-12-20 10:29:30 -0600

    validate sku & used regex in tesing

--- data/products_test.go
+++ data/products_test.go
@@ -5,8 +5,9 @@ import "testing"
 
 func TestCheckValidation(t *testing.T){
 	p:= &Product{
-		Name: "NiKlus ice cream sprinkels ",
+		Name: "NiKlus ice cream & sprinkels ",
 		Price: 2.00,
+		SKU: "shi-nob-uye",//three groups of 3 letters lowercase just like in regex
 	}
 	err:= p.Validate()
 	if err != nil{
