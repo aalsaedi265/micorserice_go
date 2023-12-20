94a045850642e7ed505e6a0a128969748e348536
Ahmed Alsaedi <aalsaedi265@gmail.com>
2023-12-20 11:08:07 -0600

    refactor

--- handlers/products.go
+++ handlers/products.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	// "encoding/json"
 	"context"
+	"fmt"
 	"log"
 	"microserices_go/data"
 	"net/http"
@@ -64,6 +65,16 @@ func (p Products) MiddlewareValidateProduct(next http.Handler) http.Handler {
 		if err != nil {
 			p.l.Println("[ERROR] deserializing product", err)
 			http.Error(rw, "Error reading product", http.StatusBadRequest)
+			return
+		}
+		// validate product
+		err = prod.Validate()
+		if err != nil{
+			p.l.Println("[ERROR] validating product", err)
+			http.Error(
+				rw, 
+				fmt.Sprintf("Error validating product: %s", err),
+				http.StatusBadRequest)
 			return
 		}
 		// add the product to the context
