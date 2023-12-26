7473ae8350eedceb0d715483020ebfdc9e6780a4
Ahmed Alsaedi <aalsaedi265@gmail.com>
2023-12-26 11:13:53 -0600

    configuration fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar';
 import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg='light' expand='lg'>
+        <Navbar.Brand href='#home'>Cafe Dearly</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+      </Navbar>
+      
     </div>
   );
 }
