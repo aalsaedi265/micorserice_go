0f91448aabd89da6322a5501f22818bddbb76afd
Ahmed Alsaedi <aalsaedi265@gmail.com>
2023-12-26 09:53:53 -0600

    react functional

--- handlers/delete.go
+++ handlers/delete.go
@@ -6,7 +6,15 @@ import (
 	"microserices_go/data"
 )
 
+// swagger:route DELETE /products/{id} products deleteProduct
+// Update a products details
+//
+// responses:
+//	201: noContentResponse
+//  404: errorResponse
+//  501: errorResponse
 
+// Delete handles DELETE requests and removes items from the database
 
 func (p *Products) Delete(rw http.ResponseWriter, r *http.Request) {
 	id := getProductID(r)
