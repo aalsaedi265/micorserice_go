d8540c66309b9c47d26fbac85713bedfdc6db81c
Ahmed Alsaedi <aalsaedi265@gmail.com>
2023-12-26 13:49:41 -0600

    front end fully cleaned

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,25 @@
 
-import React from 'react'
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/FormControl';
-import Button from 'react-bootstrap/Button';
+import React from 'react';
+import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <Navbar bg='light' expand='lg'>
-        <Navbar.Brand href='#home'>Cafe Dearly</Navbar.Brand>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">Cafe Dearly</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" >
+        <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#link">Link</Nav.Link>
           </Nav>
           <Form inline>
-            <FormControl type='text' placeholder='search' className='mr-sm-2' />
-            <Button variant='outline-success'>Search</Button>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
           </Form>
         </Navbar.Collapse>
       </Navbar>
-      
     </div>
   );
 }
