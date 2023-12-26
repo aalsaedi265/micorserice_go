23b79b9700255e423dee4dbc55731245c2ac453c
Ahmed Alsaedi <aalsaedi265@gmail.com>
2023-12-26 11:58:54 -0600

    user interface

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 
 import React from 'react'
 import Navbar from 'react-bootstrap/Navbar';
-import './App.css';
-
+import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 function App() {
   return (
@@ -12,7 +13,16 @@ function App() {
       <Navbar bg='light' expand='lg'>
         <Navbar.Brand href='#home'>Cafe Dearly</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-
+        <Navbar.Collapse id="basic-navbar-nav" >
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type='text' placeholder='search' className='mr-sm-2' />
+            <Button variant='outline-success'>Search</Button>
+          </Form>
+        </Navbar.Collapse>
       </Navbar>
       
     </div>
